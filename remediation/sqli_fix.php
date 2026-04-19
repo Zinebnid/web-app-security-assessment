73d7dc8968ffbb741d72679857c55ae6c3481575
Zineb Nid <zinebnid6@gmail.com>
2026-04-19 19:25:46 +0100

    Add SQL injection prevention in sqli_fix.php
    Implemented prepared statements to prevent SQL injection vulnerabilities.

--- remediation/sqli_fix.php
+++ remediation/sqli_fix.php
@@ -1,1 +1,20 @@
+<?php
+if( isset( $_REQUEST[ 'Submit' ] ) ) {
+    $id = $_REQUEST[ 'id' ];
 
+    // Prepared statement - safe version
+    $stmt = $GLOBALS["___mysqli_ston"]->prepare(
+        "SELECT first_name, last_name FROM users WHERE user_id = ?"
+    );
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    while( $row = $result->fetch_assoc() ) {
+        $first = $row["first_name"];
+        $last  = $row["last_name"];
+        $html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
+    }
+    $stmt->close();
+}
+?>
